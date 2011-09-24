be11dc19d91aee224ec7d31d9d937df4adeb3df9
Stefan Roeck <stefan.roeck@googlemail.com>
2011-09-24 21:23:03 +0200

    cleanup

--- tutorial/src/main/java/cleancodematters/requestfactory/tutorial/client/Tutorial.java
+++ tutorial/src/main/java/cleancodematters/requestfactory/tutorial/client/Tutorial.java
@@ -1,13 +1,8 @@
 package cleancodematters.requestfactory.tutorial.client;
 
-import cleancodematters.requestfactory.tutorial.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
@@ -70,21 +65,12 @@ public class Tutorial implements EntryPoint {
     });
 
     // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler, KeyUpHandler {
+    class MyHandler implements ClickHandler {
       /**
        * Fired when the user clicks on the sendButton.
        */
       public void onClick(ClickEvent event) {
         sendNameToServer();
-      }
-
-      /**
-       * Fired when the user types in the nameField.
-       */
-      public void onKeyUp(KeyUpEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-          sendNameToServer();
-        }
       }
 
       /**
@@ -94,10 +80,6 @@ public class Tutorial implements EntryPoint {
         // First, we validate the input.
         errorLabel.setText("");
         String textToServer = nameField.getText();
-        if (!FieldVerifier.isValidName(textToServer)) {
-          errorLabel.setText("Please enter at least four characters");
-          return;
-        }
 
         // Then, we send the input to the server.
         sendButton.setEnabled(false);
@@ -110,6 +92,5 @@ public class Tutorial implements EntryPoint {
     // Add a handler to send the name to the server
     MyHandler handler = new MyHandler();
     sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
   }
 }
