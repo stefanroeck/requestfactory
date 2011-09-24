a680d7a9ea7b0d598a73c35e1d86c4854b408f81
Stefan Roeck <stefan.roeck@googlemail.com>
2011-09-24 22:22:43 +0200

    made demo more meaningful

--- tutorial/src/main/java/cleancodematters/requestfactory/tutorial/client/Tutorial.java
+++ tutorial/src/main/java/cleancodematters/requestfactory/tutorial/client/Tutorial.java
@@ -1,5 +1,7 @@
 package cleancodematters.requestfactory.tutorial.client;
 
+import java.util.Arrays;
+
 import cleancodematters.requestfactory.tutorial.client.PizzaRequestFactory.PizzaRequestContext;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -15,49 +17,85 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.web.bindery.event.shared.SimpleEventBus;
 import com.google.web.bindery.requestfactory.shared.Receiver;
+import com.google.web.bindery.requestfactory.shared.ServerFailure;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Tutorial implements EntryPoint {
   
+  private Button sendButton;
+  private TextBox nameField;
+  private Label errorLabel;
+  private TextBox ingredientField;
+
   /**
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    
-    final Button sendButton = new Button( "Order pizza" );
-    final TextBox nameField = new TextBox();
-    final Label errorLabel = new Label();
+    sendButton = new Button( "Order pizza" );
+    nameField = new TextBox();
+    nameField.setText( "Pizza Name" );
+    ingredientField = new TextBox();
+    ingredientField.setText( "Ingredient" );
+    errorLabel = new Label();
 
     // We can add style names to widgets
-    sendButton.addStyleName("sendButton");
+    sendButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick( ClickEvent event ) {
+        orderPizza();
+      }
+    });
 
     // Add the nameField and sendButton to the RootPanel
     // Use RootPanel.get() to get the entire body element
     RootPanel.get("nameFieldContainer").add(nameField);
+    RootPanel.get("nameFieldContainer").add(ingredientField);
     RootPanel.get("sendButtonContainer").add(sendButton);
     RootPanel.get("errorLabelContainer").add(errorLabel);
 
     // Focus the cursor on the name field when the app loads
     nameField.setFocus(true);
-    nameField.selectAll();
+  }
 
-    // Create the popup dialog box
+  private void orderPizza() {
+    errorLabel.setText("");
+    sendButton.setEnabled(false);
+    
+    // Then, we send the input to the server.
+    PizzaRequestContext context = createFactory().context();
+    PizzaProxy pizza = context.create( PizzaProxy.class );
+    pizza.setName( nameField.getText() );
+    IngredientProxy ingredient = context.create( IngredientProxy.class );
+    ingredient.setName( ingredientField.getText() );
+    pizza.setIngredients( Arrays.asList( ingredient ) );
+    
+    context.save( pizza ).fire( new Receiver<Void>() {
+      @Override
+      public void onSuccess( Void arg0 ) {
+        createConfirmationDialogBox().center();
+        sendButton.setEnabled(true);
+      }
+      @Override
+      public void onFailure( ServerFailure error ) {
+        errorLabel.setText( error.getMessage() );
+        sendButton.setEnabled(true);
+      }
+    });
+    
+  }  
+  private static DialogBox createConfirmationDialogBox() {
     final DialogBox dialogBox = new DialogBox();
+    dialogBox.setModal( true );
     dialogBox.setText("Request Factory Call");
     dialogBox.setAnimationEnabled(true);
     final Button closeButton = new Button("Close");
     // We can set the id of a widget by accessing its Element
     closeButton.getElement().setId("closeButton");
-    final Label textToServerLabel = new Label();
-    final HTML serverResponseLabel = new HTML();
     VerticalPanel dialogVPanel = new VerticalPanel();
     dialogVPanel.addStyleName("dialogVPanel");
-    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-    dialogVPanel.add(textToServerLabel);
-    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-    dialogVPanel.add(serverResponseLabel);
+    dialogVPanel.add(new HTML("<b>Thanks for your order</b>"));
     dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
     dialogVPanel.add(closeButton);
     dialogBox.setWidget(dialogVPanel);
@@ -66,52 +104,9 @@ public class Tutorial implements EntryPoint {
     closeButton.addClickHandler(new ClickHandler() {
       public void onClick(ClickEvent event) {
         dialogBox.hide();
-        sendButton.setEnabled(true);
-        sendButton.setFocus(true);
       }
     });
-
-    // Create a handler for the sendButton and nameField
-    class MyHandler implements ClickHandler {
-      /**
-       * Fired when the user clicks on the sendButton.
-       */
-      public void onClick(ClickEvent event) {
-        orderPizza();
-      }
-
-      /**
-       * Send the name from the nameField to the server and wait for a response.
-       */
-      private void orderPizza() {
-        PizzaRequestContext context = createFactory().context();
-        PizzaProxy pizza = context.create( PizzaProxy.class );
-        pizza.setName( nameField.getText() );
-        
-        context.save( pizza ).fire( new Receiver<Void>() {
-
-          @Override
-          public void onSuccess( Void arg0 ) {
-            dialogBox.show();
-            sendButton.setEnabled(true);
-          }
-          
-        });
-        
-        errorLabel.setText("");
-        String textToServer = nameField.getText();
-
-        // Then, we send the input to the server.
-        sendButton.setEnabled(false);
-        textToServerLabel.setText(textToServer);
-        serverResponseLabel.setText("");
-
-      }
-    }
-
-    // Add a handler to send the name to the server
-    MyHandler handler = new MyHandler();
-    sendButton.addClickHandler(handler);
+    return dialogBox;
   }
 
   private static PizzaRequestFactory createFactory() {
--- tutorial/src/main/java/cleancodematters/requestfactory/tutorial/server/internal/PizzaDaoImpl.java
+++ tutorial/src/main/java/cleancodematters/requestfactory/tutorial/server/internal/PizzaDaoImpl.java
@@ -1,6 +1,9 @@
 package cleancodematters.requestfactory.tutorial.server.internal;
 
+import java.util.List;
+
 import cleancodematters.requestfactory.tutorial.server.PizzaDao;
+import cleancodematters.requestfactory.tutorial.server.domain.Ingredient;
 import cleancodematters.requestfactory.tutorial.server.domain.Pizza;
 
 public class PizzaDaoImpl implements PizzaDao {
@@ -13,8 +16,29 @@ public class PizzaDaoImpl implements PizzaDao {
 
   @Override
   public void save( Pizza pizza ) {
-    // TODO Auto-generated method stub
-    
+    checkPizzaAttributes( pizza );
+    checkIngredientAttributes( pizza.getIngredients() );
+  }
+
+  private static void checkPizzaAttributes( Pizza pizza ) {
+    if( isEmpty( pizza.getName() ) ) {
+      throw new IllegalArgumentException( "pizza name must not be null" );
+    }
+  }
+
+  private static void checkIngredientAttributes( List<Ingredient> ingredients ) {
+    if (ingredients.size() == 0) {
+      throw new IllegalArgumentException( "pizza cannot be empty" );
+    }
+    for (Ingredient ingredient : ingredients) {
+      if (isEmpty( ingredient.getName() )) {
+        throw new IllegalArgumentException( "ingredients should have a name, too" );
+      }
+    }
+  }
+
+  private static boolean isEmpty( String name ) {
+    return name == null || name.length() == 0;
   }
 
 }
